dc6b362d0b640700b77b385eb9688e971d576bfa
borja-rojano <borja@tapewrite.com>
2017-01-16 15:47:06 +0100

    Connecting to Mongo

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 'use strict';
 
 var express = require('express'),
-    routes = require('./app/routes/index.js');
+    routes = require('./app/routes/index.js'),
+    mongo = require('mongodb').MongoClient;
 
 var app = express();
 
-app.use('/public', express.static(process.cwd()+'/public'));
-app.use('/controllers', express.static(process.cwd()+'/app/controllers'));
+mongo.connect('mongodb://localhost:27017/mandarine', function (err, db) {
+    if (!err){
+        console.log('Connected to Database');
+    };
 
-routes(app);
+    app.use('/public', express.static(process.cwd()+'/public'));
+    app.use('/controllers', express.static(process.cwd()+'/app/controllers'));
 
-app.listen(3000, function () {
-    console.log('Listening on port 3000...');
+    routes(app);
+
+    app.listen(3000, function () {
+        console.log('Listening on port 3000...');
+    });
 });
+
